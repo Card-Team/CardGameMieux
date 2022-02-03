ccce36ed4ea62d1090864c111a5838c935d17afd
belbaz <binyam.elbaz@gmail.com>
2022-02-03 23:05:22 +0100

    Fenetre avec message terminal presque finit

---
+++ Assets/Scripts/Affichage du texte sur la console.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Affichagedutextesurlaconsole : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("mission accomplis bilel");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
