0c8a3dac64ddb4442290f6332a8d0a157603bcb1
raphaelgruet <raphaelgruet@yahoo.fr>
2022-02-13 18:46:44 +0100

    CardRenderer

--- Assets/Script/CardRenderer.cs
+++ Assets/Script/CardRenderer.cs
@@ -1,16 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using CardGameEngine;
 using CardGameEngine.Cards;
+using TMPro;
 using UnityEngine;
 
 public class CardRenderer : MonoBehaviour
 {
     public Card card;
+    public GameObject nom;
+    public GameObject description;
+    public GameObject niveauText;
+    public GameObject cout;
+    public GameObject illustration;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        nom.GetComponent<TextMeshPro>().text = card.Name.Value;
+        description.GetComponent<TextMeshPro>().text = card.Description.Value;
+        niveauText.GetComponent<TextMeshPro>().text = card.CurrentLevel.Value + "/" + card.MaxLevel;
+        cout.GetComponent<TextMeshPro>().text = card.Cost.Value + "";
+        //illustration.GetComponent<SpriteRenderer>().sprite = card.
     }
 
     // Update is called once per frame
