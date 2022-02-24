2ba90657a0d843f9ca328b51bff040733cd158db
belbaz <binyam.elbaz@gmail.com>
2022-02-24 01:40:56 +0100

    oups heberger is push

--- Assets/Script/Heberger.cs
+++ Assets/Script/Heberger.cs
@@ -3,26 +3,55 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Heberger : MonoBehaviour
 {
-
     public TMP_InputField port;
-    public const string PortH="PortHebergement";
+    public const string PortH="PortHebergement"; 
+    public GameObject panelHeberger;
+    public TMP_Text compteur;
     void Start()
     {
         if (PlayerPrefs.HasKey(PortH) && !PortH.Equals("."))
         {
             //mettre le port depuis le PlayerPrefs enregistré
-            port.text=(PlayerPrefs.GetString("PortH"));
+            port.text=(PlayerPrefs.GetString("PortHebergement"));
         }
         else
         {
             Debug.Log("Port jamais rentrer ou Vide");
         }
     }
-
+    IEnumerator onCoroutine(int i)
+    {
+        while (i>0)
+        {
+            //compteur de 1 seconde
+            yield return new WaitForSeconds(1f);
+            compteur.SetText(""+i--);
+            if (i < 10)
+            {
+                compteur.color = Color.red;
+            }
+            if (i==0)
+            {
+                panelHeberger.SetActive(false);
+            }
+        }
+    }
     public void Herberger()
     {
         PlayerPrefs.SetString(PortH,port.text);                //playerPrefs port
     }
+
+    public void AppuieHerberger()
+    {
+        panelHeberger.SetActive(true);
+        StartCoroutine(onCoroutine(60));
+    }
+    
+    public void retourHerberger()
+    {
+        panelHeberger.SetActive(false);
+    }
 }
+
