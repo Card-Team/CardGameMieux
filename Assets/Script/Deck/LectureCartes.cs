9071e4d023b98ef8c56f839a71fa23247206c5aa
belbaz <binyam.elbaz@gmail.com>
2022-02-28 23:39:57 +0100

    Affichage du nom des cartes

--- Assets/Script/Deck/LectureCartes.cs
+++ Assets/Script/Deck/LectureCartes.cs
@@ -3,16 +3,14 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class LectureCartes : MonoBehaviour
 {
-    GameObject myGO;
-    GameObject myText;
-    Canvas myCanvas;
-    Text text;
-    RectTransform rectTransform;
+    public TextMeshPro textTemplate;
+    TextMeshPro text;
     public void Start()
     {
         NomCartes();
@@ -27,16 +25,9 @@ public class LectureCartes : MonoBehaviour
         var files = Directory.GetFiles(Application.streamingAssetsPath + "/EffectsScripts/Card", "*.lua", SearchOption.AllDirectories)
             .Where(s => !(s.Contains("_")) && s.EndsWith(".lua")).ToList();
 
-        // CREER SANS Canvas !!!
-        myGO = new GameObject();
-        myGO.name = "Deck";
-        myGO.AddComponent<Canvas>();
-
-        myCanvas = myGO.GetComponent<Canvas>();
-        myCanvas.renderMode = RenderMode.ScreenSpaceOverlay;
-        myGO.AddComponent<CanvasScaler>();
-        myGO.AddComponent<GraphicRaycaster>();
-        
+        int tour = 0;
+        int posY = 3;
+        int posX = -5;
         foreach (var file in files)
         {
             //faire un tri pour le nom des fichier avec le Path en affaiblissement
@@ -45,20 +36,22 @@ public class LectureCartes : MonoBehaviour
             String fileSansPath2 = fileSansPath1.Replace(".lua","");
             Debug.Log(fileSansPath2);
             // Text
-            myText = new GameObject();
-            myText.transform.parent = myGO.transform;
-            myText.name = "Carte "+fileSansPath2;
-
-            text = myText.AddComponent<Text>();
-            text.font = (Font)Resources.Load("MyFont");
-            text.text = file;
-            text.fontSize = 100;
+            text = Instantiate(textTemplate, null);
+            text.text = fileSansPath2;      // NE MARCHE PAS POUR RENOMé le composant text
+            text.SetText(fileSansPath2);
+            text.fontSize = 5;
+            text.transform.position = new Vector3(posX, posY, 0);
+            tour+=1;
+            if (tour%5 == 0)
+            {
+                posY = 3;
+                posX += 5;
+            }
+            else
+            {
+                posY -= 1;
+            }
         }
-
-
-        // Text position
-        rectTransform = text.GetComponent<RectTransform>();
-        rectTransform.localPosition = new Vector3(0, 0, 0);
-        rectTransform.sizeDelta = new Vector2(400, 200);
+        
     }
 }
